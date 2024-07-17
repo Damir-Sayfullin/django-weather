0958529c40d72114f0ddf6b86a4d9168b50a92ee
Damir-Sayfullin <saifullin.d02@mail.ru>
2024-07-17 20:37:46 +0300

    Add form in main

--- django_weather/main/models.py
+++ django_weather/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class SearchHistory(models.Model):
+    city = models.CharField('Город', max_length=100)
+    search_count = models.IntegerField('Количество поисков', default=0)
+    last_searched = models.DateTimeField('Последний поиск', auto_now=True)
+
+    def __str__(self):
+        return str(self.city) + ' - ' + str(self.search_count) + ' - ' + str(self.last_searched)
+
+    class Meta:
+        verbose_name = 'История поиска'
+        verbose_name_plural = 'История поиска'
--- django_weather/main/views.py
+++ django_weather/main/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from .models import SearchHistory
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {}
+    if 'city' in request.GET:
+        city = request.GET['city']
+        # response = requests.get(
+        #     f'https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&hourly=temperature_2m')
+        # data = response.json()
+        # context['weather_data'] = data
+        if True:
+            context['status'] = 'ok'
+            context['weather_data'] = {'city': city, 'temperature': 20.0}
+            search_entry, _ = SearchHistory.objects.get_or_create(city=city)
+            search_entry.search_count += 1
+            search_entry.save()
+        else:
+            context['status'] = 'error'
+            context['weather_data'] = {'city': city}
+    return render(request, 'main/index.html', context)
 
 def history(request):
-    return render(request, 'main/history.html')
+    history = SearchHistory.objects.order_by('-last_searched')
+    return render(request, 'main/history.html', {'history': history})
